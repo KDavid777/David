d959b84bc42f94fe2986f0da396cbf43d6bb5653
David <prosto.david03@gmail.com>
2021-04-30 21:03:44 +0300

    hw-1

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        String sl = in.next();
+        System.out.println(sl);
+
+    }
+    }
