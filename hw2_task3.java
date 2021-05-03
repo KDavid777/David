ecc3b9478c9aceecede61ac198cfc356dc827568
KDavid777 <83469988+KDavid777@users.noreply.github.com>
2021-05-04 00:24:10 +0300

    Add files via upload

---
+++ hw2_task3.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        hashCode1();
+    }
+
+    private static void hashCode1() {
+int g=78; //Вводимое число
+if ( g % 2 ==0 )
+    System.out.println("Число четное");
+else
+    System.out.println("Число не четное");
+}
+
+    }
+
+
