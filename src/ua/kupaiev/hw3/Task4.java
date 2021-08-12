8d07fce46e91de7933ad4745352ef96e87a654bb
David <prosto.david03@gmail.com>
2021-08-12 14:47:23 +0300

    Task4

---
+++ src/ua/kupaiev/hw3/Task4.java
@@ -0,0 +1,22 @@
+package src.ua.kupaiev.hw3;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int secret = (int) (Math.random() * 100);
+        //System.out.println(secret);
+        int guess;
+        do {
+            guess = scanner.nextInt();
+            System.out.println(guess);
+            if (guess > secret) {
+                System.out.println("Много ");
+            } else if (guess < secret) {
+                System.out.println("Мало ");
+            } else System.out.println("Угадал ");
+        }
+        while (guess != secret);
+    }
+}
